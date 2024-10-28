d0c1ecd62975519ad800ed47b5264849793b5d59
yasmin silva <yasmin.nascimento50@aluno.ce.gov.br>
2024-10-28 16:58:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-const waitingLine = {}
+let property = []
+option = prompt(`
+    1 - novo movel
+    2 - sair
+
+    `)
+    const imovel = {
+        nome: 'yasmin',
+        quartos: 4,
+        banheiros: 2,
+        garagem:'sim'
+    }
+  
